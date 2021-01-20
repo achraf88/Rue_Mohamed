a7eda15c1dff11c96d35a2e9c34a97cfd0efae54
achraf88 <75786840+achraf88@users.noreply.github.com>
2021-01-20 11:18:33 +0100

    Create exercice basique.py

---
+++ exercice basique.py
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Admin
+#
+# Created:     02/12/2020
+# Copyright:   (c) Admin 2020
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+le_monde_est_plat=1
+if le_monde_est_plat:
+    print("attention a rebord")
+
+n=11
+print (n)
+n=12
+
+n=7
+print(n)
+
+
+a=3
+type(a)
+print(type(a))
+
+
+
+
